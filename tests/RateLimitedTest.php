11a4945b7106e2b41073e839eb01b0da666aa656
Freek Van der Herten <freek@spatie.be>
2019-09-29 14:05:12 +0000

    Apply fixes from StyleCI

--- tests/RateLimitedTest.php
+++ tests/RateLimitedTest.php
@@ -2,7 +2,6 @@
 
 namespace Spatie\RateLimitedMiddleware\Tests;
 
-use Closure;
 use Mockery;
 use Orchestra\Testbench\TestCase;
 use Illuminate\Support\Facades\Redis;
@@ -45,7 +44,7 @@ class RateLimitedTest extends TestCase
         $this->job->shouldReceive('fire')->times(2);
         $this->job->shouldReceive('release')->times(3);
 
-        foreach(range(1,5) as $i) {
+        foreach (range(1, 5) as $i) {
             $this->middleware->handle($this->job, $this->next);
         }
     }
