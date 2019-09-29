42e49312f3ff62a592597ee70a945f7bd836d254
freek <freek@spatie.be>
2019-09-29 15:06:13 +0200

    wip

--- tests/RateLimitedTest.php
+++ tests/RateLimitedTest.php
@@ -12,25 +12,30 @@ use Illuminate\Redis\Limiters\DurationLimiterBuilder;
 
 class RateLimitedTest extends TestCase
 {
-    /** @var Closure */
+    /** @var \Closure */
     private $next;
-    /** @var Mockery\Mock */
+
+    /** @var \Mockery\Mock */
     private $job;
-    /** @var Mockery\Mock */
+
+    /** @var \Mockery\Mock */
     private $redis;
 
-    /** @var int Fake redis lock */
+    /** @var int */
     private $callsAllowed;
 
-    /** @var RateLimited */
+    /** @var \Spatie\RateLimitedMiddleware\RateLimited */
     private $middleware;
 
     protected function setUp(): void
     {
         parent::setUp();
+
         $this->callsAllowed = 2;
+
         $this->mockRedis();
         $this->mockJob();
+
         $this->middleware = new RateLimited();
     }
 
@@ -40,7 +45,7 @@ class RateLimitedTest extends TestCase
         $this->job->shouldReceive('fire')->times(2);
         $this->job->shouldReceive('release')->times(3);
 
-        for ($i = 0; $i < 5; $i++) {
+        foreach(range(1,5) as $i) {
             $this->middleware->handle($this->job, $this->next);
         }
     }
@@ -49,13 +54,14 @@ class RateLimitedTest extends TestCase
     public function it_does_nothing_when_disabled()
     {
         $this->job->shouldReceive('fire')->times(1);
+
         $this->middleware->enabled(false)->handle($this->job, $this->next);
     }
 
     private function mockRedis(): void
     {
-        // Let's mock redis service and its lock mechanism so we can test our stuff without hassle
         $this->redis = Mockery::mock(Connection::class);
+
         $this->redis->shouldReceive('throttle')->andReturn(new DurationLimiterBuilder($this->redis, 'key'));
         $this->redis->shouldReceive('eval')->andReturnUsing(function () {
             return [
@@ -64,12 +70,14 @@ class RateLimitedTest extends TestCase
                 $this->callsAllowed--,
             ];
         });
+
         Redis::shouldReceive('connection')->andReturn($this->redis);
     }
 
     private function mockJob(): void
     {
         $this->job = Mockery::mock();
+
         $this->next = function ($job) {
             $job->fire();
         };
